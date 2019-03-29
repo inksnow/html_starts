a1e98e4ddb2e2af83b14407a62a9efd8194abf82
inksnow <546039057@qq.com>
2019-03-29 16:07:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="./styleAndJS/style.css" rel="stylesheet">
+<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
+<script type="text/javascript" src="./jquery/jquery-3.3.1.js"></script>
+<title>Starts</title>
+</head>
+
+<body  style="overflow-x:hidden;overflow-y:hidden" id= "body">
+
+		<article>
+		<div >
+		<img class="img_bg" id = "img_bg" src="./images/bg_1.png">
+		<p id="time_text">12:00:00</p>
+		<canvas class="canvas_1" id="canvas_1" onmouseout="mouseOut()" onmousemove="mouseMove(event)">
+			</canvas>
+		</div>
+		</article>
+		
+
+	
+</body>
+<script type="text/javascript" src="./styleAndJS/CarouselPicture.js"></script>
+<script type="text/javascript" src="./styleAndJS/starts.js"></script>
+</html>
